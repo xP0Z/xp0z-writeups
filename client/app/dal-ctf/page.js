5d0b8d71e67478e2c034b51f539beb295f4f08da
xP0Z <damn.me1137@gmail.com>
2026-06-09 17:47:12 +0530

    Reverse arrangement of writeups for DalCTF

--- client/app/dal-ctf/page.js
+++ client/app/dal-ctf/page.js
@@ -2,6 +2,6 @@ import { getWriteupsByEvent } from '../../lib/markdown';
 import CitCtfClient from './ClientPage';
 
 export default function CITCTF() {
-  const writeups = getWriteupsByEvent("DalCTF");
+  const writeups = getWriteupsByEvent("DalCTF").reverse();
   return <CitCtfClient writeups={writeups} eventName="DalCTF" />;
 }
